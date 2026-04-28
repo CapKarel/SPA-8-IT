a920d2a0bb240b1b777fdbf0231575ed0f8b8c8b
CapKarel <cap.karel@zak.spspb.cz>
2026-04-28 12:42:00 +0200

    Add Skills function to display skills list

--- views/skills.js
+++ views/skills.js
@@ -1,1 +1,10 @@
-
+export function Skills() {
+    return `
+        <h2>Dovednosti</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Java</li>
+        </ul>
+    `;
+}
