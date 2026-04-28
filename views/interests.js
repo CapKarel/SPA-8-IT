b29bba0b49abf414c5138e2a26939cda3e4db67c
CapKarel <cap.karel@zak.spspb.cz>
2026-04-28 12:41:21 +0200

    Add Interests component to display user interests

--- views/interests.js
+++ views/interests.js
@@ -1,1 +1,10 @@
+export function Interests() {
+    const interests = ["Programování", "idk", "idk2"];
 
+    return `
+        <h2>Znájmy</h2>
+        <ul>
+            ${interests.map(i => `<li>${i}</li>`).join("")}
+        </ul>
+    `;
+}
